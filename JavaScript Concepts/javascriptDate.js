30d625350e5a1fd7f0515d8b8f911fe0b915f7aa
Sourabh Patil <92427348+Sourabh0407@users.noreply.github.com>
2021-10-13 15:47:32 +0530

    Create javascriptDate.js

---
+++ JavaScript Concepts/javascriptDate.js
@@ -0,0 +1,7 @@
+<script>  
+var date=new Date();  
+var day=date.getDate();  
+var month=date.getMonth()+1;  
+var year=date.getFullYear();  
+document.write("<br>Date is: "+day+"/"+month+"/"+year);  
+</script>  
