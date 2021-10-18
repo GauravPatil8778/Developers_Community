2f7a87e6c980bd2b352998e7a553cbc1bf212af7
RameshLad <92710276+RameshLad@users.noreply.github.com>
2021-10-18 11:57:44 +0530

    Create constructor1.js

---
+++ JavaScript Concepts/constructor1.js
@@ -0,0 +1,10 @@
+<script>  
+class Employee {  
+  constructor() {  
+    this.id=101;  
+    this.name = "Martin Roy";  
+  }   
+}  
+var emp = new Employee();  
+document.writeln(emp.id+" "+emp.name);  
+</script>  
