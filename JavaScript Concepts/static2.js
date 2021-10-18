49ccc756b1005af4c32aed86072c6886542c5028
RameshLad <92710276+RameshLad@users.noreply.github.com>
2021-10-18 12:03:24 +0530

    Create static2.js

---
+++ JavaScript Concepts/static2.js
@@ -0,0 +1,15 @@
+<script>  
+class Test  
+{  
+  static display1()  
+  {  
+    return "static method is invoked"  
+  }  
+  static display2()  
+  {  
+    return "static method is invoked again"  
+  }  
+}  
+document.writeln(Test.display1()+"<br>");  
+document.writeln(Test.display2());  
+</script>  
